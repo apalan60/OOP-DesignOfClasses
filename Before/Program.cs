7ef9c2a64a29529affe0de2e68c7286be4cbcd60
apalan60 <apalan60@gmail.com>
2024-06-24 08:54:38 +0800

    Add sample class 'Before' to compare

---
+++ Before/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Globalization;
+
+namespace OOP_Design_of_Classes;
+
+public class Book
+{
+    private string _title = string.Empty;
+    private string _publisher = string.Empty;
+    private int _edition;
+
+    public Book(int edition, string title, string publisher, DateOnly publicationDate, CultureInfo culture)
+    {
+        Edition = edition;
+        Title = title;
+        Publisher = publisher;
+        PublicationDate = publicationDate;
+        Culture = culture;
+    }
+
+    public CultureInfo Culture { get; set; }
+    
+    public string Title
+    {
+        get => _title;
+        set => _title = !string.IsNullOrWhiteSpace(value)? value: throw new ArgumentException(nameof(Title));
+    }
+
+    public string Publisher
+    {
+        get => _publisher;
+        set => _publisher = !string.IsNullOrWhiteSpace(value)? value: throw new ArgumentException(nameof(Publisher));
+    }
+
+    public int Edition
+    {
+        get => _edition;
+        set => _edition = value > 0 ? value : throw new ArgumentException(nameof(Edition));
+    }
+
+    public DateOnly PublicationDate { get; set; }
+
+
+    
+}
