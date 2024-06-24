0263c110f2d134cc45f511fc2c1bf953a0dcd47d
apalan60 <apalan60@gmail.com>
2024-06-24 22:33:15 +0800

    Add sample class 'add readonly List for accessing

--- Before/Program.cs
+++ Before/Program.cs
@@ -7,16 +7,17 @@ public class Book
     private string _title = string.Empty;
     private string _publisher = string.Empty;
     private int _edition;
-
-    public Book(int edition, string title, string publisher, DateOnly publicationDate, CultureInfo culture)
+    
+    public Book(int edition, string title, string publisher, DateOnly publicationDate, CultureInfo culture, List<string> authorCollection)
     {
         Edition = edition;
         Title = title;
         Publisher = publisher;
         PublicationDate = publicationDate;
         Culture = culture;
+        _authorCollection = authorCollection;
     }
-
+    
     public CultureInfo Culture { get; set; }
     
     public string Title
@@ -39,6 +40,27 @@ public class Book
 
     public DateOnly PublicationDate { get; set; }
 
-
+    private readonly List<string> _authorCollection;
+    public IEnumerable<string> Authors => _authorCollection;
     
+    public void AppendAuthor(string author)
+    {
+        if (string.IsNullOrWhiteSpace(author))
+        {
+            throw new ArgumentException(author, nameof(author));
+        }
+        _authorCollection.Add(author);
+    }
+    
+    public bool RemoveAuthor(string author) => _authorCollection.Remove(author);
+    
+    public bool MoveAuthorTo(int index, string author)
+    {
+        if (index < 0 || index >= _authorCollection.Count)
+        {
+            return false;
+        }
+        _authorCollection[index] = author;
+        return true;
+    }
 }
