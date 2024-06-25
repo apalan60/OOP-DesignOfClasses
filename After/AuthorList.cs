49c8b418cedfcfb4b64c1348f034f71961aff6e0
apalan60 <apalan60@gmail.com>
2024-06-25 21:03:36 +0800

    Add cultureInfo

--- After/AuthorList.cs
+++ After/AuthorList.cs
@@ -1,10 +1,13 @@
 ﻿using System.Collections;
+using System.Globalization;
 
 namespace After;
 
-public class AuthorList(IEnumerable<string> authors) : IEnumerable<string>
+public class AuthorList(IEnumerable<string> authors, CultureInfo cultureInfo) : IEnumerable<string>
 {
     private readonly List<string> _authorCollection = authors.Where(IsValidAuthor).ToList();
+    
+    private CultureInfo CultureInfo { get; } = cultureInfo;
 
     public void AppendAuthor(string author)
     {
@@ -12,7 +15,13 @@ public class AuthorList(IEnumerable<string> authors) : IEnumerable<string>
         _authorCollection.Add(author);
     }
     
-    public bool RemoveAuthor(string author) => _authorCollection.Remove(author);
+    public bool RemoveAuthor(string author) =>  _authorCollection.Remove(author);
+    
+    private Func<string, bool> FilterFor(string filter) => 
+        author => string.Compare(author, filter, CultureInfo, CompareOptions.IgnoreCase) == 0;
+    
+    public void AuthorsToUpperCase() => _authorCollection
+        .ForEach(author => _authorCollection[_authorCollection.IndexOf(author)] = CultureInfo.TextInfo.ToUpper(author));
     
     public bool MoveAuthorTo(int index, string author)
     {
@@ -37,4 +46,9 @@ public class AuthorList(IEnumerable<string> authors) : IEnumerable<string>
     public IEnumerator<string> GetEnumerator() => _authorCollection.GetEnumerator();
 
     IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+}
+
+public class Author
+{
+    
 }
--- After/Program.cs
+++ After/Program.cs
@@ -1,4 +1,2 @@
 ﻿using System.Globalization;
 using After;
-
-var book = new Book(1, "C# 9 and .NET 5", "barkley", new DateOnly(2020, 11, 20), CultureInfo.InvariantCulture, new AuthorList(["Mark J. Price"]));
